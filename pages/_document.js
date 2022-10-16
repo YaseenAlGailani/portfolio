870d8826823bc5f11a0efd1e43ab44d616280daa
Yaseen AlGailani <yaseen.algailani@gmail.com>
2022-10-16 18:09:51 +1100

    theme based on system or local storage

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Script from "next/script";
 
 export default function Document() {
   return (
-    <Html lang="en" className="dark">
+    <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
