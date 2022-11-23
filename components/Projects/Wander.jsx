f6cb01b7439f43adbac70c801d6c884fa2ad99ac
Yaseen AlGailani <yaseen.algailani@gmail.com>
2022-11-23 17:50:36 +1100

    refactor:add link

--- components/Projects/Wander.jsx
+++ components/Projects/Wander.jsx
@@ -6,16 +6,28 @@ export default function Wander() {
     <>
       <div className={styles.projectBody}>
         <p>
-          <strong>Stack: </strong>Reactjs, Nextjs, Nodejs, TypeScript, Tailwindcss, PostgreSQL, and a bunch of libraries
+          <strong>Stack: </strong>Reactjs, Nextjs, Nodejs, TypeScript,
+          Tailwindcss, PostgreSQL, and a bunch of libraries
         </p>
         <Hr className="my-4" />
         <p>
-          This one is a side project of mine. A trip planner where you can put all your itenrary in one place.
+          This one is a side project of mine. A trip planner where you can put
+          all your itenrary in one place.
         </p>
-        <p>It&apos;s a work in progress, get in touch if you would like to learn more.</p>
+        <p>
+          It&apos;s a work in progress, get in touch if you would like to learn
+          more.
+        </p>
+        <a
+          className={styles.visitLink}
+          href="https://www.wander-planner.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Visit website
+        </a>
       </div>
-      <footer>
-      </footer>
+      <footer></footer>
     </>
   );
 }
